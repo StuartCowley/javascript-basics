6172449dbfc4e447b13dd24232fa656857728e1a
Cora Marcu <cora.marcu@gmail.com>
2022-02-18 19:29:02 +0000

    Finished reverseWordsInArray

--- src/arrays.js
+++ src/arrays.js
@@ -1,6 +1,6 @@
 const getNthElement = (index, array) => {
   // your code here
-  if(index > array.length - 1) {
+  if (index > array.length - 1) {
     index -= array.length;
   }
   return array[index];
@@ -8,35 +8,63 @@ const getNthElement = (index, array) => {
 
 const arrayToCSVString = array => {
   // your code here
-  
+  return array.reduce((total, element) => `${total},${element}`);
 };
 
 const csvStringToArray = string => {
   // your code here
+  const newArr = [];
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
   // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
   // your code here
+  const secondArr = [element];
+  return array.concat(secondArr);
 };
 
 const removeNthElement = (index, array) => {
   // your code here
+  array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
   // your code here
+  return numbers.map(number => {
+    return number.toString();
+  });
 };
 
 const uppercaseWordsInArray = strings => {
   // your code here
+  return strings.map(element => {
+    return element.toUpperCase();
+  });
 };
 
 const reverseWordsInArray = strings => {
   // your code here
+  // 1. map() through 'strings' array to reach each word one by one so you can do things to them in turn (reverse letters)
+  // 2. for each word, turn it into an array of characters
+  const arrayOfArraysOfCharacters = strings.map(string => string.split(''));
+  // 3. map through the new array of arrays and reverse() each array of characters
+  const arrayOfArraysOfReversedCharacters = arrayOfArraysOfCharacters.map(
+    array => array.reverse()
+  );
+  // 4. map through the new array and reduce each array of characters to a string (you get the reversed words into a new array)
+  const finalArrayOfReversedWords = arrayOfArraysOfReversedCharacters.map(
+    array => {
+      return array.reduce((reversedWord, currentLetter) => {
+        return reversedWord + currentLetter;
+      });
+    }
+  );
+  return finalArrayOfReversedWords;
 };
 
 const onlyEven = numbers => {
