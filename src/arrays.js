bb416b8645386394c7780b65ea4aa6a9358961eb
Ben Quinn <benjamesquinn@gmail.com>
2022-03-01 17:29:43 +0000

    finish array and objects

--- src/arrays.js
+++ src/arrays.js
@@ -1,86 +1,91 @@
 const getNthElement = (index, array) => {
-  if (index > array.length -1) {
+  if (index > array.length - 1) {
     index -= array.length;
   }
   return array[index];
 };
 
-const arrayToCSVString = array => {
-  return array.toString();
-};
+const arrayToCSVString = array => array.toString();
 
-const csvStringToArray = string => {
-  return string.split(',')
-};
+const csvStringToArray = string => string.split(',');
 
 const addToArray = (element, array) => {
   array.push(element);
 };
 
-const addToArray2 = (element, array) => {
-  let arr = array.splice(array.length, 0, element);
-  return arr;
-};
+const addToArray2 = (element, array) => array.concat(element);
 
-const removeNthElement = (index, array) => {
-  return array.splice(index, 1);
-  };
+const removeNthElement = (index, array) => array.splice(index, 1);
 
 const numbersToStrings = numbers => {
-  let arr = numbers.map((item) => {
+  const arr = numbers.map(item => {
     return item.toString();
   });
   return arr;
 };
 
 const uppercaseWordsInArray = strings => {
-  let arr = strings.map((item) => {
+  const arr = strings.map(item => {
     return item.toUpperCase();
   });
   return arr;
 };
 
 const reverseWordsInArray = strings => {
-  let arr = strings.map((item) => {
-    return item.split('').reverse().join('');
+  const arr = strings.map(item => {
+    return item
+      .split('')
+      .reverse()
+      .join('');
   });
   return arr;
 };
 
 const onlyEven = numbers => {
- let arr = numbers.filter((item) => {
-   return item % 2 === 0;
+  const arr = numbers.filter(item => {
+    return item % 2 === 0;
   });
   return arr;
 };
 
 const removeNthElement2 = (index, array) => {
-  array.splice(index, 1);
-  return array.splice();
-  };
+  const arr = array.filter((_, elementIndex) => elementIndex !== index);
+  return arr;
+};
 
 const elementsStartingWithAVowel = strings => {
-  let arr = strings.filter((items) => {
-  return items[0].includes('a') || items[0].includes('A') || items[0].includes('e') || items[0].includes('E') || items[0].includes('i') || items[0].includes('I') || items[0].includes('o') || items[0].includes('O') || items[0].includes('u') || items[0].includes('U')
+  const arr = strings.filter(items => {
+    return (
+      items[0].includes('a') ||
+      items[0].includes('A') ||
+      items[0].includes('e') ||
+      items[0].includes('E') ||
+      items[0].includes('i') ||
+      items[0].includes('I') ||
+      items[0].includes('o') ||
+      items[0].includes('O') ||
+      items[0].includes('u') ||
+      items[0].includes('U')
+    );
   });
-  return arr; 
+  return arr;
 };
 
 const removeSpaces = string => {
-  let str = string.split(' ').join('');
+  const str = string.split(' ').join('');
   return str;
 };
 
 const sumNumbers = numbers => {
-  let arr = numbers.reduce((total, item) => {
+  const arr = numbers.reduce((total, item) => {
     return item + total;
   }, 0);
   return arr;
 };
 
 const sortByLastLetter = strings => {
-  let arr = strings.sort((a, b) => {
-    return a.charCodeAt(a.length -1) - b.charCodeAt(b.length -1);
+  const arr = strings.sort((a, b) => {
+    return a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1);
   });
   return arr;
 };
